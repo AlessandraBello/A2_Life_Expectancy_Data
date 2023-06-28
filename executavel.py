31fea0e4a5ce11569e7623c687402b22e19d0588
giovanna <gigigrossimonteiro@hotmail.com7>
2023-06-28 15:27:24 -0300

    Criação do gráfico 4 (investimento do PIB)

--- executavel.py
+++ executavel.py
@@ -4,3 +4,5 @@ import leitura_arquivo as la
 grafico_1 = cp.grafico_de_dispercao("expectativa_de_vida_escolaridade.html", "Life expectancy ", "Schooling", 
                                     la.dados_grafico1, "blue", 0.6, "white" , "Expectativa de Vida e Escolaridade", 
                                     "Expectativa de Vida (em anos)", "Escolaridade (em anos)")
+
+grafico_4 = cp.grafico_de_dispercao("exp_de_vida_investimento_saude.html", "Life expectancy", "percentage expenditure", la.dados_grafico4, "red", 0.5, "white", "Expectativa de Vida e Investimento do PIB em Saúde (por país)", "Expectativa de Vida (em anos)", "Investimento no Setor Saúde (em % do PIB)")
