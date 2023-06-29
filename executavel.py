0e66c906fe964a1d3b603f029337d14d1e858be0
Alessandra Belló <alessandrabellosoares@gmail.com>
2023-06-29 12:09:34 -0300

    Criaçao do segundo gráfico com elementos basicos

--- executavel.py
+++ executavel.py
@@ -2,8 +2,12 @@ import criar_plot as cp
 import leitura_arquivo as la
 
 grafico_1 = cp.grafico_de_dispercao("expectativa_de_vida_escolaridade.html", "Life expectancy ", "Schooling", 
-                                    la.dados_grafico1, "blue", 0.6, "white" , "Expectativa de Vida e Escolaridade", 
+                                    la.dados_grafico1, "blue", 0.6, "white" , "Expectativa de Vida e Escolaridade, 2015", 
                                     "Expectativa de Vida (em anos)", "Escolaridade (em anos)")
 
-grafico_4 = cp.grafico_de_dispercao("mortalidade_infantil_polio.html", "under-five deaths ", "Polio", la.dados_grafico4, "red", 0.5, "white", "Mortalidade Infantil e Vacinação contra Polio em 2015", "Mortalidade Infantil", "Vacinação contra poliomelite (em %)")
+grafico_2 = cp.grafico_de_linha("evolucao_expectativa_brasil.html", "Year", "Life expectancy ", la.dados_grafico2, "white", "Expectativa de vida no Brasil, de 2000 a 2015", "Ano", "Expectativa de Vida")
+
+grafico_4 = cp.grafico_de_dispercao("mortalidade_infantil_polio.html", "under-five deaths ", "Polio", la.dados_grafico4, 
+                                    "red", 0.5, "white", "Mortalidade Infantil e Vacinação contra Polio em 2015", 
+                                    "Mortalidade Infantil", "Vacinação contra poliomelite (em %)")
 
--- leitura_arquivo.py
+++ leitura_arquivo.py
@@ -4,11 +4,17 @@ from bokeh.plotting import ColumnDataSource
 #lê o banco de dados csv
 df = pd.read_csv("Life Expectancy Data.csv")
 
-#seleciona as colunas de expectativa de vida e escolaridade
-expectativa_escolaridade = df[["Life expectancy ", "Schooling"]]
+#seleciona as colunas de expectativa de vida e escolaridade do ano de 2015
+expectativa_escolaridade = df[["Life expectancy ", "Schooling"]][df["Year"]==2015]
 
 #transforma os dados de expectativa de vida e escolaridade em um objeto ColumnDataSource
 dados_grafico1 = ColumnDataSource(data=expectativa_escolaridade)
+
+#seleciona somente as linhas do Brasil e as colunas ano e expectativa de vida
+dados_brasil = df[["Year","Life expectancy "]][df["Country"]=="Brazil"]
+
+#transforma os dados em um objeto ColumnDataSource
+dados_grafico2 = ColumnDataSource(data=dados_brasil)
 
 #seleciona as colunas de morte antes dos 5 anos e vacinação contra a poliomelite
 morte_infantil_poliomelite = df[["under-five deaths ", "Polio"]][df ["Year"] == 2015]
