9dfddfe12b752345b012385d98f2342eba588659
Alessandra Belló <alessandrabellosoares@gmail.com>
2023-06-29 20:49:51 -0300

    Criaçao do modulo pra escrever textos e um teste

--- executavel.py
+++ executavel.py
@@ -1,6 +1,7 @@
 import criar_plot as cp
 import leitura_arquivo as la
 import layout
+import textos
 from bokeh.models import FactorRange
 
 grafico_1 = cp.grafico_de_dispercao("Life expectancy ", "Schooling", la.dados_grafico1, "blue", 0.6, "white" , 
@@ -13,7 +14,7 @@ grafico_2 = cp.grafico_de_linha("Year", "Life expectancy ", la.dados_grafico2, "
 grafico_3 = cp.grafico_de_barras( FactorRange(*la.doenca_ano), "eixo_x", "medias", la.dados_grafico3, "blue", "white", 
                                  "Média de vacinação nos anos de 2000 e 2015", "Doenças", "Média de vacinação (em %)")
 
-graficos_alessandra = layout.agrupar("Alessandra.html", grafico_1, None, grafico_2, None, grafico_3, None)
+graficos_alessandra = layout.agrupar("Alessandra.html", grafico_1, textos.texto_grafico_1, grafico_2, None, grafico_3, None)
 
 grafico_4 = cp.grafico_de_dispercao("under-five deaths ", "Polio", la.dados_grafico4, "red", 0.5, "white", 
                                     "Mortalidade Infantil e Vacinação contra Polio em 2015", "Mortalidade Infantil", 
---
+++ textos.py
@@ -0,0 +1,3 @@
+from bokeh.models import Paragraph
+
+texto_grafico_1 = Paragraph(text = "Neste primeiro grafico vemos a expectativa de vida, em anos, e o tempo que as pessoas passaram na escola, também em anos. Optei por um gráfico de dispersão.")
