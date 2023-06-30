0dc43a42d996cbf5f94c4c96cc3f0ba466780560
Manuelakist <manuelaskist11@gmail.com>
2023-06-29 21:31:46 -0300

    Adicionei a funçao do gráfico 7

---
+++ graficos_789.py
@@ -0,0 +1,60 @@
+import pandas as pd
+from bokeh.plotting import figure, output_file, show
+from bokeh.models import ColumnDataSource, HoverTool, PanTool, WheelZoomTool, ResetTool
+import leitura_arquivo as la
+
+
+def grafico7():
+
+    # Define o arquivo de saída
+    output_file("expectativa_media_continente.html")
+
+    # Define uma cor para cada continente
+    continent_colors = {"Africa": "#E74C3C", "Asia": "#F39C12", "Europe": "#3498DB", "North America": "#DD1491", "Oceania": "#8E44AD", "South America": "#1ABC9C"}
+
+    # Cria o gráfico e configura as ferramentas usadas
+    grafico7 = figure(tools = [HoverTool(tooltips=[("Ano", "@x"), ("Expectativa de Vida", "@y")]), PanTool(), WheelZoomTool(), ResetTool(), ])
+
+    # Plota as linhas para cada continente
+    for continent in la.media_expectativa_por_continente["Continent"].unique():
+        df_continent = la.media_expectativa_por_continente[la.media_expectativa_por_continente["Continent"] == continent]
+        grafico7.line(df_continent["Year"], df_continent["Life expectancy "], color = continent_colors[continent], legend_label = continent, line_width = 2)
+
+    # Configurações gerais do gráfico:
+
+    # Barra de ferramentas
+    grafico7.toolbar.logo = None
+    grafico7.toolbar.autohide = True
+    # Dimensões
+    grafico7.width = 640
+    grafico7.height = 480
+    # Título e rótulos de linha
+    grafico7.title.text = "Média de Expectativa de Vida por Ano para Cada Continente"
+    grafico7.title.text_font = "Arial Black"
+    grafico7.title.text_font_size = "18px"
+    grafico7.title.align = "center"
+    grafico7.xaxis.axis_label = "Ano"
+    grafico7.xaxis.axis_label_text_font = "Verdana"
+    grafico7.xaxis.axis_label_text_font_size = "15px"
+    grafico7.yaxis.axis_label = "Expectativa de Vida"
+    grafico7.yaxis.axis_label_text_font = "Verdana"
+    grafico7.yaxis.axis_label_text_font_size = "15px"
+    grafico7.yaxis.major_label_orientation = "vertical"
+    # Cores
+    grafico7.background_fill_color = "white"
+    # Eixos
+    grafico7.xaxis.ticker.num_minor_ticks = 2
+    grafico7.yaxis.ticker.num_minor_ticks = 5
+    # Grid
+    grafico7.ygrid.minor_grid_line_dash = "dotted"
+    grafico7.ygrid.grid_line_color = "#F2DFF2"
+    grafico7.xgrid.grid_line_color = "#F2DFF2"
+    grafico7.ygrid.minor_grid_line_color = "#F2DFF2"
+    grafico7.xgrid.minor_grid_line_color = "#F2DFF2"
+    grafico7.ygrid.minor_grid_line_alpha = 0.5
+    grafico7.xgrid.minor_grid_line_alpha = 0.6
+    # Legenda
+    grafico7.legend.location = "bottom_left"
+
+    return show(grafico7)
+
