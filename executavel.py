4c4caac3c45a18165299d23f69ead5ba757554d0
Alessandra Belló <alessandrabellosoares@gmail.com>
2023-06-30 21:38:07 -0300

    Partes da Giovanna adicionadas ao código

--- executavel.py
+++ executavel.py
@@ -2,6 +2,8 @@ import criar_plot_alessandra as cp
 import leitura_arquivo as la
 import layout
 import textos
+import giovanna_plots as gp
+import grid_giovanna as gg
 
 #gera o gráfico 1, bastando apenas fornecer os parâmetros necessários
 grafico_1 = cp.grafico_de_dispercao((50,90), (3,22), "Life expectancy ", "Schooling", la.dados_grafico1_1, 
@@ -22,3 +24,15 @@ grafico_3 = cp.grafico_de_barras(la.doencas_e_medias_por_ano["doenca"], (0,110),
 #gera um único html com os 3 gráficos gerados e os 3 textos correspondentes
 graficos_alessandra = layout.agrupar("Alessandra.html", grafico_1, textos.texto_grafico_1, grafico_2, 
                                      textos.texto_grafico_2, grafico_3, textos.texto_grafico_3)
+
+#chama o gráfico 4
+grafico4 = gp.grafico_4()
+
+#chama o grid/gráfico 5
+grafico5 = gg.grid_giovanna()
+
+#chama o gráfico 6
+grafico6 = gp.grafico_6()
+
+#criando o html com os gráficos e os textos
+graficos_giovanna = layout.agrupar("Giovanna.html", grafico4, textos.texto_grafico_4, grafico5, textos.texto_grafico_5, grafico6, textos.texto_grafico_6)
--- leitura_arquivo.py
+++ leitura_arquivo.py
@@ -57,3 +57,33 @@ doencas_e_medias_por_ano = {"doenca": ["Poliomielite", "Hepatite B", "Difteria"]
 
 #converte os dados em um objeto ColumnDataSource
 dados_grafico3 = ColumnDataSource(data=doencas_e_medias_por_ano)
+
+#seleciona as colunas de anos e magreza extrema de 5-9 anos e tira a media de acordo com o continente
+anos_magreza59_continente = df.groupby(["Year", "Continent"])[" thinness 5-9 years"].mean().reset_index()
+
+#transforma os dados de anos e magreza extrema de 5-9 anos em um objeto ColumnDataSource
+dados_grafico4 = ColumnDataSource(data=anos_magreza59_continente)
+
+#seleciona as colunas de anos e alcoolismo no brasil
+alcoolismo_brasil = df[["Year","Alcohol"]][df["Country"]=="Brazil"][df["Year"] != 2015]
+
+#transforma os dados de anos e alcoolismo no brasil  em um objeto ColumnDataSource
+dados_grafico5 = ColumnDataSource(data = alcoolismo_brasil)
+
+#seleciona as colunas de anos e alcool e tira a media de acordo com o continente
+anos_alcool_continente = df[df["Year"] != 2015].groupby(["Year", "Continent"])["Alcohol"].mean().reset_index()
+
+#transforma os dados de anos e alcoolismo por continente  em um objeto ColumnDataSource
+dados_grafico52 = ColumnDataSource(data = anos_alcool_continente)
+
+#seleciona as colunas de paises e alcool do continente europeu
+anos_alcool_europa = df[(df["Continent"] == "Europe") & (df["Year"] == 2007)][["Country", "Alcohol"]]
+
+#transforma os dados de países e alcoolismo europeu em um objeto ColumnDataSource
+dados_grafico53 = ColumnDataSource(data = anos_alcool_europa)
+
+#seleciona as colunas de taxa de mortalidade e idh de 2015
+mortalidade_idh = df[["Adult Mortality","Income composition of resources", "Country"]][df["Year"] == 2015]
+
+#transforma os dados de taxa de mortalidade e idh em um objeto ColumnDataSource
+dados_grafico6 = ColumnDataSource(data = mortalidade_idh)
