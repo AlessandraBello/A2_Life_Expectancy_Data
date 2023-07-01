fcc778b109c51df7f43174af5fa0f59af9de4acd
Alessandra Belló <alessandrabellosoares@gmail.com>
2023-06-30 22:05:23 -0300

    Modulos de geração graficos adicionados

---
+++ grid_giovanna.py
@@ -0,0 +1,23 @@
+from bokeh.io import output_file
+from giovanna_plots import grafico_51, grafico_52, grafico_53
+from bokeh.layouts import gridplot
+
+#criando as variáveis dos gráficos
+grafico_5_1 = grafico_51()
+grafico_5_2 = grafico_52()
+grafico_5_3 = grafico_53()
+
+#criando o gridplot na função
+def grid_giovanna():
+
+    output_file("grid_alcoolismo.html")
+
+    grid_alcoolismo1 = gridplot([[grafico_5_2, grafico_5_1]], sizing_mode = "fixed", width = 500, height= 400)
+
+    grid_alcoolismo1.toolbar.logo = None
+
+    grid_alcoolismo2 = gridplot([[grid_alcoolismo1],[grafico_5_3]], sizing_mode = "fixed", width = 1000, height= 500)
+
+    grid_alcoolismo2.toolbar.logo = None
+
+    return grid_alcoolismo2
